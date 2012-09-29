f294141e510e8677bb5154795184f875938da52e
loosethisskin <loosethisskin@gmail.com>
2012-09-29 00:06:32 -0400

    Set the default toolchain to linaro 2012.09

--- envsetup.sh
+++ envsetup.sh
@@ -141,7 +141,7 @@ function setpaths()
 
     # default toolchain directory
     if [ -z "$TOOLCHAIN_DIR" ]; then
-      export TOOLCHAIN_DIR=linaro-4.7-2012.08
+      export TOOLCHAIN_DIR=linaro-4.7-2012.09
     fi
     # The gcc toolchain does not exists for windows/cygwin. In this case, do not reference it.
     export ANDROID_EABI_TOOLCHAIN=
